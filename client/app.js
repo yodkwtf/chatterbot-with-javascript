730ea113bdfa197bbf8c44a7ed0d0cef661f55fd
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-01-24 11:22:06 +0530

    server url updated

--- client/app.js
+++ client/app.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
   loader(botMessageContainer);
 
   // get bot's response from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatterbot-dk.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
